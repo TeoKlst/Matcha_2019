7f01417f38e750a5228104b8fa94b06647ff8ddb
TeoKlst <37896596+TeoKlst@users.noreply.github.com>
2019-09-27 16:56:22 +0200

    27/09/19
    +Tags added to .db

--- matcha/matcha.py
+++ matcha/matcha.py
@@ -13,18 +13,21 @@ class User(db.Model):
     id          = db.Column(db.Integer, primary_key=True)
     firstname   = db.Column(db.String(20), nullable=False)
     lastname    = db.Column(db.String(20), nullable=False)
+    age         = db.Column(db.Integer, nullable=False)
     username    = db.Column(db.String(20), unique=True, nullable=False)
     email       = db.Column(db.String(120), unique=True, nullable=False)
     password    = db.Column(db.String(60), nullable=False)
     gender      = db.Column(db.String(20), nullable=False)
     sexualpref  = db.Column(db.String(20), nullable=False)
     biography   = db.Column(db.Text, nullable=False)
+    famerating  = db.Column(db.Integer, nullable=False, default=0)
     image_file  = db.Column(db.String(20), nullable=False, default='default.jpg')
-    userchecks  = db.Column(db.String(100), default='')  
+    userchecks  = db.Column(db.String(100), default='')
+    tags        = db.Column(db.String(20), default='') 
     posts       = db.relationship('Post', backref='author', lazy=True)
     likes       = db.relationship('Like', backref='userlikes', lazy=True)
     messages    = db.relationship('Message', backref='usermessages', lazy=True)
-    user_images =  db.relationship('Image', backref='userimages', lazy=True)
+    user_images = db.relationship('Image', backref='userimages', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
@@ -49,7 +52,6 @@ class Message(db.Model):
 
 class Image(db.Model):
     id          = db.Column(db.Integer, primary_key=True)
-    # Image_Relation = 1 - 5 =>
     # 1 = Profile photo , 2 - 5 = Being other photos
     image_rel   = db.Column(db.Integer, nullable=False)
     image_file  = db.Column(db.String(20), nullable=False, default='default.jpg')
@@ -58,11 +60,19 @@ class Image(db.Model):
     def __repr__(self):
         return f"Message('{self.content}', '{self.date_sent}', '{self.username}')"
 
+class Tags(db.Model):
+    id          = db.Column(db.Integer, primary_key=True)
+    title       = db.Column(db.String(100), nullable=False)
+    user_list   = db.Column(db.Text, default='')
+
+    def __repr__(self):
+        return f"Post('{self.id}', '{self.title}', '{self.user_list}')"
+
 class Post(db.Model):
     id          = db.Column(db.Integer, primary_key=True)
     title       = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content     = db.Column(db.Text, nullable=False) 
+    content     = db.Column(db.Text, nullable=False)
     user_id     = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
