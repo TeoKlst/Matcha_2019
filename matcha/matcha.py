c59ce731a30a30969622c311a21f8b98bbbce119
TeoKlst <37896596+TeoKlst@users.noreply.github.com>
2019-09-27 11:54:24 +0200

    27/09/19
    +Forms added
    +Flash messaged added
    +Login,Register.html added

--- matcha/matcha.py
+++ matcha/matcha.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '02f223f600f6e93454be89fb8ec9c78c'
 
 posts = [
     {
@@ -16,12 +19,33 @@ posts = [
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', posts = posts)
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title = 'About')
+    return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'test@gmail.com' and form.password.data == 'password':
+            flash(f'You have logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
