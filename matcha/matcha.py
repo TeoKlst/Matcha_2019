6b91b0dfff7c061877248df0e5829d465119af5a
TeoKlst <37896596+TeoKlst@users.noreply.github.com>
2019-09-27 16:44:00 +0200

    27/09/19
    +Database added
    +Gitignore .db files

--- matcha/matcha.py
+++ matcha/matcha.py
@@ -1,9 +1,73 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '02f223f600f6e93454be89fb8ec9c78c'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' 
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id          = db.Column(db.Integer, primary_key=True)
+    firstname   = db.Column(db.String(20), nullable=False)
+    lastname    = db.Column(db.String(20), nullable=False)
+    username    = db.Column(db.String(20), unique=True, nullable=False)
+    email       = db.Column(db.String(120), unique=True, nullable=False)
+    password    = db.Column(db.String(60), nullable=False)
+    gender      = db.Column(db.String(20), nullable=False)
+    sexualpref  = db.Column(db.String(20), nullable=False)
+    biography   = db.Column(db.Text, nullable=False)
+    image_file  = db.Column(db.String(20), nullable=False, default='default.jpg')
+    userchecks  = db.Column(db.String(100), default='')  
+    posts       = db.relationship('Post', backref='author', lazy=True)
+    likes       = db.relationship('Like', backref='userlikes', lazy=True)
+    messages    = db.relationship('Message', backref='usermessages', lazy=True)
+    user_images =  db.relationship('Image', backref='userimages', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Like(db.Model):
+    id          = db.Column(db.Integer, primary_key=True)
+    username    = db.Column(db.String(20), unique=True, nullable=False)
+    user_id     = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Like('{self.id}', '{self.user_id}', '{self.username}')"
+
+class Message(db.Model):
+    id          = db.Column(db.Integer, primary_key=True)
+    username    = db.Column(db.String(20), unique=True, nullable=False)
+    content     = db.Column(db.Text, nullable=False)
+    date_sent   = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id     = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Message('{self.content}', '{self.date_sent}', '{self.username}')"
+
+class Image(db.Model):
+    id          = db.Column(db.Integer, primary_key=True)
+    # Image_Relation = 1 - 5 =>
+    # 1 = Profile photo , 2 - 5 = Being other photos
+    image_rel   = db.Column(db.Integer, nullable=False)
+    image_file  = db.Column(db.String(20), nullable=False, default='default.jpg')
+    user_id     = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Message('{self.content}', '{self.date_sent}', '{self.username}')"
+
+class Post(db.Model):
+    id          = db.Column(db.Integer, primary_key=True)
+    title       = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content     = db.Column(db.Text, nullable=False) 
+    user_id     = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+            return f"Post('{self.title}', '{self.date_posted}')"
+        
 
 posts = [
     {
