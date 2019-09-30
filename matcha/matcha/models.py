c18a0271eade7218481d2c14d37a22b38b1a5cba
TeoKlst <37896596+TeoKlst@users.noreply.github.com>
2019-09-30 22:01:07 +0200

    30/09/19
    +Hashable password
    +User creation
    +Login management
    +Logout added
    +User creation validators added
    +Account.html

--- matcha/matcha/models.py
+++ matcha/matcha/models.py
@@ -1,26 +1,30 @@
 from datetime import datetime
-from matcha import db
+from matcha import db, login_manager
+from flask_login import UserMixin
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id          = db.Column(db.Integer, primary_key=True)
-    firstname   = db.Column(db.String(20), nullable=False)
-    lastname    = db.Column(db.String(20), nullable=False)
-    age         = db.Column(db.Integer, nullable=False)
+    # firstname   = db.Column(db.String(20), nullable=False)
+    # lastname    = db.Column(db.String(20), nullable=False)
+    # age         = db.Column(db.Integer, nullable=False)
     username    = db.Column(db.String(20), unique=True, nullable=False)
     email       = db.Column(db.String(120), unique=True, nullable=False)
     password    = db.Column(db.String(60), nullable=False)
-    gender      = db.Column(db.String(20), nullable=False)
-    sexualpref  = db.Column(db.String(20), nullable=False)
-    biography   = db.Column(db.Text, nullable=False)
-    famerating  = db.Column(db.Integer, nullable=False, default=0)
+    # gender      = db.Column(db.String(20), nullable=False)
+    # sexualpref  = db.Column(db.String(20), nullable=False)
+    # biography   = db.Column(db.Text, nullable=False)
+    # famerating  = db.Column(db.Integer, nullable=False, default=0)
     image_file  = db.Column(db.String(20), nullable=False, default='default.jpg')
-    userchecks  = db.Column(db.String(100), default='')
-    tags        = db.Column(db.String(20), default='') 
+    # userchecks  = db.Column(db.String(100), default='')
+    # tags        = db.Column(db.String(20), default='') 
     posts       = db.relationship('Post', backref='author', lazy=True)
-    likes       = db.relationship('Like', backref='userlikes', lazy=True)
-    messages    = db.relationship('Message', backref='usermessages', lazy=True)
-    user_images = db.relationship('Image', backref='userimages', lazy=True)
+    # likes       = db.relationship('Like', backref='userlikes', lazy=True)
+    # messages    = db.relationship('Message', backref='usermessages', lazy=True)
+    # user_images = db.relationship('Image', backref='userimages', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
